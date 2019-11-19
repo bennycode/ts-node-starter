fa548a64c9dc2b7b60a2cd1b9cb2ba52b13e2f10
Benny Neugebauer <bn@bennyn.de>
2019-11-19 17:45:07 +0100

    refactor: Replace Travis CI with GitHub Actions

--- nyc.config.coverage.js
+++ nyc.config.coverage.js
@@ -1,11 +1,11 @@
-const defaultConfig = require("./nyc.config");
+const defaultConfig = require('./nyc.config');
 
 module.exports = Object.assign({}, defaultConfig, {
   all: true,
   branches: 100,
-  "check-coverage": true,
+  'check-coverage': true,
   functions: 100,
   lines: 100,
-  reporter: ["html"],
-  statements: 100
+  reporter: ['html'],
+  statements: 100,
 });
--- nyc.config.js
+++ nyc.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   all: false,
-  "check-coverage": false,
-  exclude: ["**/*.d.ts", "**/*.test*.ts", "**/index.ts", "**/start.ts"],
-  extension: [".ts"],
-  include: ["src/**/*.ts"],
-  "per-file": false,
-  reporter: ["text-summary"],
-  require: ["ts-node/register"]
+  'check-coverage': false,
+  exclude: ['**/*.d.ts', '**/*.test*.ts', '**/index.ts', '**/start.ts'],
+  extension: ['.ts'],
+  include: ['src/**/*.ts'],
+  'per-file': false,
+  reporter: ['text-summary'],
+  require: ['ts-node/register'],
 };
