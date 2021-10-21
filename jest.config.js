3cc131c93a895d47f08608b432880306bfc21e99
Benny Neugebauer <bn@bennyn.de>
2021-10-21 18:02:09 +0200

    fix configs

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("@jest/types").Config.InitialOptions} */
-const config = {
-  preset: 'ts-jest',
-  setupFilesAfterEnv: [],
-  testEnvironment: 'node',
-  testRegex: '\\.test\\.tsx?$',
-};
-
-module.exports = config;
