c231d12370ac785f3446023d6cb4eed968a6c83f
Benny Neugebauer <mail@bennycode.com>
2023-08-02 13:03:32 +0200

    formatting

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,9 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      // coverage thresholds
       branches: 100,
-
       functions: 100,
-
       include: ['**/*.{ts,tsx}', '!**/*.d.ts', '!**/cli.ts', '!**/index.ts', '!**/start*.ts'],
-
       lines: 100,
       provider: 'v8',
       reporter: ['html', 'lcov', 'text'],
