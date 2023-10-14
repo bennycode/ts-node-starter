3756c95db94ebe14f3cef09df1dd0f19ff5c5e42
Benny Neugebauer <mail@bennycode.com>
2023-10-14 19:09:25 +0200

    chore: Showcase filename & dirname

--- src/start.ts
+++ src/start.ts
@@ -1,1 +1,7 @@
-console.log('Hello, World!');
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(`Hello from directory: ${__dirname}`);
