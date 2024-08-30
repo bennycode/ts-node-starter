83236340101b34b56246fffdb74a51d7bd9ec2fd
Benny Neugebauer <mail@bennycode.com>
2024-08-30 14:35:17 +0200

    fix: Replace tsimp with ts-node

--- src/start.ts
+++ src/start.ts
@@ -2,6 +2,6 @@ import path from 'node:path';
 import {fileURLToPath} from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirsname = path.dirname(__filename);
 
 console.log(`Hello from directory: ${__dirname}`);
