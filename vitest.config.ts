ada36e0972ac7ac4659b29b4c3bd6e133d97cc2f
Benny Neugebauer <mail@bennycode.com>
2025-01-08 15:51:48 +0100

    fix: Allow Vitest to process the "accessor" keyword

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,11 @@
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
+  esbuild: {
+    // Allows using the "accessor" keyword in TypeScript:
+    // https://github.com/vitest-dev/vitest/issues/5976#issuecomment-2190804966
+    target: 'es2022',
+  },
   test: {
     bail: 1,
     coverage: {
