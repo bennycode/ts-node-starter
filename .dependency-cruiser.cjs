1c529172f05bca24ade833edbd47e6817296b881
Benny Neugebauer <mail@bennycode.com>
2025-03-25 10:59:15 +0100

    chore: Testing formatting

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -46,7 +46,7 @@ module.exports = {
       },
     },
     {
-      name:     'not-to-deprecated',
+      name: 'not-to-deprecated',
       comment:
         'This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later ' +
         'version of that module, or find an alternative. Deprecated modules are a security risk.',
