27ed3645b80b1bf508b709069024dc90dbd2bd3b
Benny Neugebauer <mail@bennycode.com>
2025-05-14 14:19:43 +0200

    feat: Add sample Express.js app

--- src/start.ts
+++ src/start.ts
@@ -1,1 +1,19 @@
-console.log(`Hello from directory: ${import.meta.dirname}`);
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_, res) => {
+  res.setHeader('Content-Type', 'text/html');
+
+  for (let i = 1; i <= 10; i++) {
+    res.write(`<p>${i}</p>`);
+  }
+
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Hello from directory: ${import.meta.dirname}`);
+  console.log(`Server running at http://localhost:${port}`);
+});
